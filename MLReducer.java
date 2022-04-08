bb16bc998d841b7227a047be086cf5bfa611a62e
andrewdoesstuff <ahhannie56@gmail.com>
2022-04-08 18:23:52 -0500

    initial commit

---
+++ MLReducer.java
@@ -0,0 +1,33 @@
+package project;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/* 
+ * To define a reduce function for your MapReduce job, subclass 
+ * the Reducer class and override the reduce method.
+ * The class definition requires four parameters: 
+ *   The data type of the input key (which is the output key type 
+ *   from the mapper)
+ *   The data type of the input value (which is the output value 
+ *   type from the mapper)
+ *   The data type of the output key
+ *   The data type of the output value
+ */   
+public class MLReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+  /*
+   * The reduce method runs once for each key received from
+   * the shuffle and sort phase of the MapReduce framework.
+   * The method receives a key of type Text, a set of values of type
+   * IntWritable, and a Context object.
+   */
+  @Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+	  
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# MovieLens Ratings Analyis
+## CSC 4740 Project
+
+Our goal is to use the data from these two sets to analyze trends in films over time.
+By drawing from these two specific sets, we will be able to find trends from both general audiences and critics.
+This could then show if these two different spheres of film criticism follow each other or diverge from one another over time.
+
+### Datasets:
+
+[MovieLens Ratings](https://grouplens.org/datasets/movielens/)  
+[Rotten Tomatoes Critic Reviews](https://www.kaggle.com/stefanoleone992/rotten-tomatoes-movies-and-critic-reviews-dataset)
+
